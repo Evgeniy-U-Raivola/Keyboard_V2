bc5e2f88ace0269e45146c7daa825ac1565d0fed
Evgeniy-U-Raivola <EPU47@yandex.ru>
2025-03-14 09:33:54 +0300

    шлифовка задания ввод текста

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
-
 
 public class Main extends JFrame implements KeyListener {
-    Scanner T=new Scanner(System.in);
     JLabel label;
-    JTextField tf;
+    static JTextField tf;
     FileWriter writer=new FileWriter("C:\\08_JAVA\\Keyboard_V2\\src\\output.txt",true);
 
     Main() throws IOException {
@@ -21,25 +18,23 @@ public class Main extends JFrame implements KeyListener {
              this.setVisible(true);
              this.setAlwaysOnTop(true);
              this.setLayout(null);
-             this.T=T;
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
              label=new JLabel ("Ввод  строки:");
              label.setBounds(0,0,100,20);
              label.setOpaque(true);
              label.setBackground(Color.ORANGE);
              this.add(label);
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-             tf=new JTextField(50);
-             tf.setBounds(0,30,500,50);
-             tf.setBackground(Color.white);
-             tf.addKeyListener(this);
-             this.add(tf);
-             tf.requestFocus();
                  }
-
 
     public static void main(String[] args) throws IOException {
         Main m1=new Main();
+
+        tf=new JTextField(50);
+        tf.setBounds(0,30,500,50);
+        tf.setBackground(Color.white);
+        tf.addKeyListener(m1);
+        m1.add(tf);
+        tf.requestFocus();
               }
 
     @Override
